88e6cc784f7d2998add8252c01467aa85fc1691c
aklightall <156939550+aklightall@users.noreply.github.com>
2024-04-29 04:10:28 +0000

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
  Project 2 - Array of Objects
- Name: 
+ Name: Ariana Lighthall
  Comments: 
  */
 
@@ -11,16 +11,42 @@
 ***/
 
 // Global Variables go here
+var sprites = new Array(10);
+var sprite1, sprite2
 
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600 x 400 pixel drawing canvas
+  sprite1 = new Sprite(100, 100);
+  console.log ("sprite 1");
+  console.log (Sprite1)
+  sprite2 = new Sprite(100, 200);
+  console.log("sprite 2")
+  console.log(sprite2);
 
+  for(let i = 0; i < sprites.length; i++){
+    sprites[i]=new Sprite(random(width), random(height));
+    console.log(sprites[i]);
+  }
 
 }
 
 function draw(){
   background(200); //light gray background
+  for(let i = 0; i < sprites.length; i++){
+    sprites[i].display()
+    sprites[i].move();
+  }
+  function Blob(){
+    this.x = random(width)
+    this.y = random (height)
+    this.xSpeed = random (-2, 2);
+    thiys,ySpeed= random (-2, 2)
+
+    this.move = function (){
+
+    }
+  }
   
 }
 
